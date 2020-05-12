086190dbc2151cda6d4fa6e95de4a3de1dc1f797
unknown <Admin@LAPTOP-CELIEN.hexa.local>
2020-05-12 20:52:15 +0200

    some updates

--- build/script.js
+++ build/script.js
@@ -1,5 +1,15 @@
 function showOverlay() {
     overlay = document.getElementById('overlay')
+    if (overlay.classList.contains('visible')) {
+        setTimeout(() => {
+            alert('Votre préinscription a bien été prise en compte !')
+        }, 500)
+    }
+    overlay.classList.toggle('visible')
+}
+
+function closeOverlay() {
+    overlay = document.getElementById('overlay')
     overlay.classList.toggle('visible')
 }
 
@@ -7,7 +17,6 @@ $('.radio-group .radio').click(function () {
     $(this).parent().find('.radio').removeClass('selected');
     $(this).addClass('selected');
     var val = $(this).attr('data-value');
-    //alert(val);
     $(this).parent().find('input').val(val);
 });
 
@@ -16,4 +25,15 @@ function openNav() {
 }
 function closeNav() {
     document.querySelector(".mobNav").style.width = "0%";
-}   
+}
+
+function contact() {
+    setTimeout(() => {
+        alert('Votre message a été envoyé !')
+    }, 500)
+}
+function subscribe() {
+    setTimeout(() => {
+        alert('Votre inscription a été prise en compte !')
+    }, 500)
+}
